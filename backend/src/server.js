aed06bbfb2aed71012aec0b15ac000d6de4bd6e5
Deepak-2514 <kjdeepak2514@gmail.com>
2025-11-29 18:18:53 +0530

    ..

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import { ENV } from './lib/env.js'
 
 const app = express()
 
-app.get('/health', (req, res) => {
+app.get('/healh', (req, res) => {
   res.status(200).json({ msg: 'Api is running' })
 })
 
